8734195446ce4d149aa069ea402a1fea77593371
David P. Chassin <david.chassin@me.com>
2026-02-01 22:38:26 -0800

    Update tests.py

--- loads/tests.py
+++ loads/tests.py
@@ -14,17 +14,13 @@ import pandas as pd
 
 from weather import Weather
 from fips import Counties
-from loads.residential import Residential
-from loads.commercial import Commercial
-from loads.industry import Industry
-from loads.agriculture import Agriculture
-from loads.cast import Cast
+from loads.total import Total
 
 total = None
 year = 2020
 refresh = False
 precision = 3
-tests = ["WECC","CAISO"]
+# tests = ["CAISO"]
 
 def get_total_load(state,county,refresh=refresh,progress=lambda *x:None):
     progress(f"Residential({state=},{county=},{refresh=})")
@@ -52,13 +48,18 @@ if __name__ == "__main__":
     pd.options.display.width = None
     pd.options.display.max_columns = None
 
+    if len(sys.argv) > 1:
+        tests = sys.argv[1:]
+    else:
+        tests = ["CAISO"]
+
     if not "GITHUB_ACTIONS" in os.environ and "WECC" in tests:
         # WECC load test
         print("Testing WECC...")
         wecc_counties = Counties(use_index="RO").loc["WECC"].reset_index().set_index(["ST","COUNTY"]).sort_index()
         for state,county in wecc_counties.index.values:
             print("Reading",county,state,end="... ",flush=True)
-            get_total_load(state,county,progress=lambda x:print(x[:3].lower(),flush=True,end=" "))
+            Total(state,county,year)
             print("... ok")
 
     # CAISO load test
@@ -70,8 +71,8 @@ if __name__ == "__main__":
             )/1000
         caiso_data.columns = ["elec_actual_MW"]
         caiso_data.index = pd.date_range(
-            start="2020-01-01 00:00:00-08:00",
-            end="2020-12-31 23:59:59-08:00",
+            start=f"{year}-01-01 00:00:00-08:00",
+            end=f"{year}-12-31 23:59:59-08:00",
             freq="1h",
             ).tz_convert("UTC")
         caiso_data.index.name="timestamp"
@@ -80,66 +81,29 @@ if __name__ == "__main__":
         # print(caiso_data)
 
         # Test CAISO load
-        caiso_counties = Counties(use_index="ST").loc["CA"].reset_index().set_index(["ST","COUNTY"]).sort_index()
-        # print(caiso.loc["CA"])
-        exclude = [
-            "Del Norte","Siskiyou", # PAC
-            "Modoc", # BPAT
-            "El Dorado","Sacramento", # BANC
-            "Mariposa","Merced", # TIDC
-            "Los Angeles", # LADWP
-            "Imperial", # IID (except eastern Riverside)
-            ]
-        caiso_counties = caiso_counties.reset_index().set_index("COUNTY").drop(exclude,axis=0).reset_index().set_index(["ST","COUNTY"])
+        caiso_counties = Counties(use_index="REGION").loc["CAISO"].reset_index().set_index(["ST","COUNTY"]).sort_index()
+        # # print(caiso.loc["CA"])
+        # exclude = [
+        #     "Del Norte","Siskiyou", # PAC
+        #     "Modoc", # BPAT
+        #     "El Dorado","Sacramento", # BANC
+        #     "Mariposa","Merced", # TIDC
+        #     "Los Angeles", # LADWP
+        #     "Imperial", # IID (except eastern Riverside)
+        #     ]
+        # caiso_counties = caiso_counties.reset_index().set_index("COUNTY").drop(exclude,axis=0).reset_index().set_index(["ST","COUNTY"])
         errors = 0
         for state,county in caiso_counties.index.values:
 
             print("Reading",county,state,end="...",flush=True)
 
-            data = get_total_load(state,county,progress=lambda x:print(x[:3].lower(),flush=True,end=" "))
+            data = Total(state,county,year)
             if total is None:
                 total = data.copy()
             else:
                 total += data
 
-            # check MW totals
-            error_index = []
-            for source in ["elec","nonelec"]:
-
-                # check that MW enduses add up to MW totals
-                enduse = data[[f"{source}_{x}_MW" for x in ["baseload","cooling","heating"]]]
-                subtotal = enduse.sum(axis=1)
-                diff = (subtotal - data[f"{source}_total_MW"]).round(precision)
-                if ( diff != 0 ).any():
-                    data[f"{source}_total_err"] = diff
-                    print(f"ERROR [loads.tests]: {source} MW total load test failed!",file=sys.stderr)
-                    error_index.extend(diff[diff!=0].index)
-                    errors += 1
-
-            # check that MW total and DG add up to net
-            diff = (data[f"elec_net_MW"] - data[f"elec_total_MW"] - data[f"elec_dg_MW"]).round(precision)
-            if ( diff != 0 ).any():
-                data["elec_net_err"] = diff
-                print("ERROR [loads.tests]: elec MW net load test failed!",file=sys.stderr)
-                error_index.extend(diff[diff!=0].index)
-                errors += 1
-
-            # save errors, if any
-            file = f"{state}_{county}_errors.csv"
-            if error_index:
-                """Save errors"""
-                data.loc[sorted(list(set(error_index)))].to_csv(file,index=True,header=True)
-            else:
-                try:
-                    os.remove(file)
-                except FileNotFoundError:
-                    pass
-                print("ok")
-
-        if errors:
-            print(f"{errors} error found!")
-        else:
-            print("No errors found")
+            print("ok")
 
         net = total["elec_net_MW"]
         net_sum = net.sum()/1e6
@@ -158,7 +122,7 @@ if __name__ == "__main__":
 
         print(f"\n                       Loads      CAISO {year}     Error")
         print(   "                     ----------   ----------   -----------------")
-        print(  f"Total energy (TWh)   {net_sum:8.3f}     {act_sum:8.3f}     {err_sum:8.3f} ({err_sum/act_sum*100:+5.1f})")
-        print(  f"Max power (GW)       {net_max:8.3f}     {act_max:8.3f}     {err_max:8.3f} ({err_max/act_max*100:+5.1f})")
-        print(  f"Min power (GW)       {net_min:8.3f}     {act_min:8.3f}     {err_min:8.3f} ({err_min/act_min*100:+5.1f})")
+        print(  f"Total energy (TWh)   {net_sum:8.3f}     {act_sum:8.3f}     {err_sum:8.3f} ({err_sum/act_sum*100:+5.1f}%)")
+        print(  f"Max power (GW)       {net_max:8.3f}     {act_max:8.3f}     {err_max:8.3f} ({err_max/act_max*100:+5.1f}%)")
+        print(  f"Min power (GW)       {net_min:8.3f}     {act_min:8.3f}     {err_min:8.3f} ({err_min/act_min*100:+5.1f}%)")
 
